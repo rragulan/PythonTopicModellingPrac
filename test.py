e461f268094d13fb8042090b706f1aa9c428d1cf
Ragulan <ragulan550@gmail.com>
2018-11-19 19:44:51 -0500

    trying graphing and fixing mallet

--- test.py
+++ test.py
@@ -22,14 +22,22 @@ import networkx as nx
 
 warnings.filterwarnings("ignore",category=DeprecationWarning)
 
+import nltk
+nltk.download('stopwords')
+nltk.download('words')
+
 # NLTK Stop words
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 
-print(stop_words)
+from nltk.corpus import words
+eng_words = words.words('en')
+
 
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
+import requests
 
 array = []
 
@@ -44,10 +52,10 @@ def remove_stopwords(texts):
     return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
 
 getTextFromJson('02.json')
-getTextFromJson('03.json')
-getTextFromJson('04.json')
-getTextFromJson('05.json')
-getTextFromJson('06.json')
+# getTextFromJson('03.json')
+# getTextFromJson('04.json')
+# getTextFromJson('05.json')
+# getTextFromJson('06.json')
 # getTextFromJson('07.json')
 # getTextFromJson('08.json')
 # getTextFromJson('09.json')
@@ -55,7 +63,7 @@ getTextFromJson('06.json')
 data = array
 
 # Remove links
-data = [re.sub('https', '', sent) for sent in data]
+data = [re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', sent) for sent in data]
 
 # Remove new line characters
 data = [re.sub('\s+', ' ', sent) for sent in data]
@@ -63,13 +71,35 @@ data = [re.sub('\s+', ' ', sent) for sent in data]
 # Remove distracting single quotes
 data = [re.sub("\'", "", sent) for sent in data]
 
+# Remove hashtag
+data = [re.sub("#", "", sent) for sent in data]
+
+#remove non-ascii characters need to fix leaves blank lines
+#data = [re.sub(r'[^\x00-\x7F]+','', sent) for sent in data]
+
+def remove_non_english_words(tweets):
+     return [[word for word in simple_preprocess(str(tweet)) if word in eng_words] for tweet in tweets]
+
+def remove_empty_sent(tweets) :
+    res = []
+    for tweet in tweets:
+        if len(tweet) != 0:
+            res.append(tweet)
+    return res
+
+data = remove_non_english_words(data)
+
+data = remove_empty_sent(data)
+
 def sent_to_words(sentences):
     for sentence in sentences:
         yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
 
+
 data_words = list(sent_to_words(data))
 
 data_words_no_stp = remove_stopwords(data_words)
+
 
 print(data_words_no_stp[:1])
 
@@ -101,7 +131,7 @@ corpus = [id2word.doc2bow(text) for text in texts]
 print([[(id2word[id], freq) for id, freq in cp] for cp in corpus[:1]])
 
 # Build LDA model
-lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,id2word=id2word,num_topics=3, random_state=100,update_every=1,chunksize=100,passes=10,alpha='auto',per_word_topics=True)
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,id2word=id2word,num_topics=6, random_state=100,update_every=1,chunksize=100,passes=10,alpha='auto',per_word_topics=True)
 
 # Print the Keyword in the 10 topics
 pprint(lda_model.print_topics())
@@ -120,75 +150,93 @@ print('\nPerplexity: ', lda_model.log_perplexity(corpus))  # a measure of how go
 vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
 pyLDAvis.save_html(vis, 'LDA_Visualization.html')
 
+os.environ.update({'MALLET_HOME':r'C:/Users/Ragulan/Documents/PythonTopicModellingPractice/mallet-2.0.8/mallet-2.0.8/'})
 
-# mallet_path = dir_path + '/mallet-2.0.8/mallet-2.0.8/bin/mallet' # update this path
-# ldamallet = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=5, id2word=id2word)
-# # Show Topics
-# pprint(ldamallet.show_topics(formatted=False))
+mallet_path = 'C:\\Users\Ragulan\\Documents\\PythonTopicModellingPractice\\mallet-2.0.8\\mallet-2.0.8\\bin\\mallet' # update this path
+print(mallet_path)
+ldamallet = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=5, id2word=id2word)
+# Show Topics
+pprint(ldamallet.show_topics(formatted=False))
 
-# # Compute Coherence Score
-# coherence_model_ldamallet = CoherenceModel(model=ldamallet, texts=data_words_no_stp, dictionary=id2word, coherence='c_v')
+# Compute Coherence Score
+# coherence_model_ldamallet = CoherenceModel(model=ldamallet, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
 # coherence_ldamallet = coherence_model_ldamallet.get_coherence()
 # print('\nCoherence Score: ', coherence_ldamallet)
 
 
-# def format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data):
-#     # Init output
-#     sent_topics_df = pd.DataFrame()
+def format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data):
+    # Init output
+    sent_topics_df = pd.DataFrame()
 
-#     # Get main topic in each document
-#     for i, row in enumerate(ldamodel[corpus]):
-#         print(row)
-#         row = sorted(row, key=lambda x: (x[1]), reverse=True)
-#         # Get the Dominant topic, Perc Contribution and Keywords for each document
-#         for j, (topic_num, prop_topic) in enumerate(row):
-#             if j == 0:  # => dominant topic
-#                 wp = ldamodel.show_topic(topic_num)
-#                 topic_keywords = ", ".join([word for word, prop in wp])
-#                 sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
-#             else:
-#                 break
-#     sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+    # Get main topic in each document
+    for i, row in enumerate(ldamodel[corpus]):
+        print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
 
-#     # Add original text to the end of the output
-#     contents = pd.Series(texts)
-#     sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
-#     return(sent_topics_df)
-
-
-# df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data)
-
-# # Format
-# df_dominant_topic = df_topic_sents_keywords.reset_index()
-# df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
-
-# # Show
-# df_dominant_topic.head(10)
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
 
 
-# # Number of Documents for Each Topic
-# topic_counts = df_topic_sents_keywords['Dominant_Topic'].value_counts()
+df_topic_sents_keywords = format_topics_sentences(ldamodel=ldamallet, corpus=corpus, texts=data)
 
-# # Percentage of Documents for Each Topic
-# topic_contribution = round(topic_counts/topic_counts.sum(), 4)
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
 
-# # Topic Number and Keywords
-# topic_num_keywords = df_topic_sents_keywords[['Dominant_Topic', 'Topic_Keywords']]
+# Show
+df_dominant_topic.head(10)
 
-# # Concatenate Column wise
-# df_dominant_topics = pd.concat([topic_num_keywords, topic_counts, topic_contribution], axis=1)
 
-# # Change Column names
-# df_dominant_topics.columns = ['Dominant_Topic', 'Topic_Keywords', 'Num_Documents', 'Perc_Documents']
+# Number of Documents for Each Topic
+topic_counts = df_topic_sents_keywords['Dominant_Topic'].value_counts()
 
-# # Show
-# df_dominant_topics
+# Percentage of Documents for Each Topic
+topic_contribution = round(topic_counts/topic_counts.sum(), 4)
+
+# Topic Number and Keywords
+topic_num_keywords = df_topic_sents_keywords[['Dominant_Topic', 'Topic_Keywords']]
+
+# Concatenate Column wise
+df_dominant_topics = pd.concat([topic_num_keywords, topic_counts, topic_contribution], axis=1)
+
+# Change Column names
+df_dominant_topics.columns = ['Dominant_Topic', 'Topic_Keywords', 'Num_Documents', 'Perc_Documents']
+
+# Show
+df_dominant_topics
 
 
 #Networkx
 G = nx.Graph()
-G.add_node(1)
-G.add_nodes_from([2, 3])
+# G.add_node(1)
+# G.add_nodes_from([2, 3])
+
+count = 0
+for index, row in df_dominant_topic.iterrows():
+    G.add_node(count)
+    G.node[count]["text"] = row["Text"]
+    G.node[count]["topic"] = row["Dominant_Topic"]
+    count += 1
+    if count > 300:
+        break
+        
+for i in range(300):
+    for j in range(i,300):
+        if G.node[i]["topic"] == G.node[j]["topic"] and len(G.edges(i)) < 3:
+            G.add_edge(i,j)
+        
 nx.draw(G, with_labels=True, font_weight='bold')
 plt.show()
+     
 
